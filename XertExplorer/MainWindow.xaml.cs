b9f768273c3b71e0cfa3f190f7f2d92b890e22d4
Chris <ccuddohy@gmail.com>
2020-09-08 09:01:14 -0400

    starting to add functionality and look

--- XertExplorer/MainWindow.xaml.cs
+++ XertExplorer/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using XertClient;
 
 namespace XertExplorer
 {
@@ -23,6 +27,31 @@ namespace XertExplorer
 		public MainWindow()
 		{
 			InitializeComponent();
+			try
+			{
+				List<XertWorkout> Wos = GetWorkouts("workouts.json");
+				if (Wos.Count > 0)
+				{
+					ListViewWorkouts.ItemsSource = Wos;
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("An exception was caught: " + ex.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+			}
+		}
+
+
+		/// <summary>
+		/// for development, will load workouts from file. The file is copied to bin on build as set in the VS properties Build Action
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		static List<XertWorkout> GetWorkouts(string path)
+		{
+			string JSONtxt = File.ReadAllText(path);
+			List<XertWorkout> wkouts = JsonSerializer.Deserialize<List<XertWorkout>>(JSONtxt);//JsonSerializer.DeserializeObject<List<XertWorkout>>(JSONtxt);
+			return wkouts;
 		}
 	}
 }
