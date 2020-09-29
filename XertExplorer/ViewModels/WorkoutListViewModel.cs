09068b9364dd8ee0b37415485c3dc05608b9e055
Chris Cuddohy <ccuddohy@gmail.com>
2020-09-29 17:02:35 -0400

    changed filter to focus filter including variable name changes, added timespan constants for duration filtering, aded WorkoutListFilteredSorted to replace WorkoutListFiltered, added DurationFilters starting to implement duratin filtering which is not yet flushed out

--- XertExplorer/ViewModels/WorkoutListViewModel.cs
+++ XertExplorer/ViewModels/WorkoutListViewModel.cs
@@ -8,6 +8,7 @@ using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -19,10 +20,11 @@ namespace XertExplorer.ViewModels
 	public class WorkoutListViewModel : INotifyPropertyChanged
 	{
 		IXertClient _client;
-		public ICommand FilterCommand { get; set; }
+		public ICommand FocusFilterCommand { get; set; }
+		public ICommand DurationFilterCommand { get; set; }
 		public ICommand SortCommand { get; set; }
 		public ICommand ToggleSortDirection { get; set; }
-		
+
 
 		private enum SortingParam
 		{
@@ -35,9 +37,12 @@ namespace XertExplorer.ViewModels
 		};
 		SortingParam _currentSortingParam;
 
-		
+
 		const string _pathSortOrderAssendingImage = @"/ViewComponents/ArrowDown.jpg"; // the file properties need to be set to resource so they can be used at run-time
 		const string _pathSortOrderDescendingImage = @"/ViewComponents/ArrowUp.jpg"; // the file properties need to be set to resource so they can be used at run-time
+		readonly TimeSpan _lowLimTimeSpan = new TimeSpan(0, 45, 0);
+		readonly TimeSpan _highLimTimeSpan = new TimeSpan(2, 30, 0);
+		readonly TimeSpan _minuteIncrementTimeSpan = new TimeSpan(0, 15, 0);
 
 		private string _pathSortOrderDirectionImage;
 		public string PathSortOrderDirectionImage
@@ -91,7 +96,7 @@ namespace XertExplorer.ViewModels
 		private void ToggleSortOrderDirection(object parameter)
 		{
 			AssendingSortDirection = !AssendingSortDirection;
-			ApplySorting();
+			ApplySorting(WorkoutListFilteredSorted);
 		}
 
 		/// <summary>
@@ -135,7 +140,12 @@ namespace XertExplorer.ViewModels
 		/// <summary>
 		/// This is a List of all the workouts for the user. It is unfiltered and not sorted.
 		/// </summary>
-		private List<IXertWorkout> _allWorkOuts;
+		private List<IXertWorkout> _allWorkouts;
+
+		/// <summary>
+		/// a subset of _allWorkouts that is filtered to include only the focus parameters in FocusFilters
+		/// </summary>
+		private List<IXertWorkout> _focusFilteredWorkouts;
 
 		/// <summary>
 		/// simply returns a count of all the unfiltered workouts for the user.
@@ -144,13 +154,13 @@ namespace XertExplorer.ViewModels
 		{
 			get
 			{
-				if (null == _allWorkOuts)
+				if (null == _allWorkouts)
 				{
 					return 0;
 				}
-				return _allWorkOuts.Count();
+				return _allWorkouts.Count();
 			}
-			private set	{}
+			private set { }
 		}
 
 		private string _username;
@@ -168,7 +178,7 @@ namespace XertExplorer.ViewModels
 		}
 
 		private string _statusMessage;
-		
+
 		/// <summary>
 		/// An indication of login operations and workout loading status
 		/// </summary>
@@ -203,37 +213,40 @@ namespace XertExplorer.ViewModels
 		}
 
 		public ICommand LoginCommand { get; }
-	
-		public List<string> Filters { get; set; }
+
+		public List<string> FocusFilters { get; set; }
+		public List<string> DurationFilters { get; set; }
+
+		
 
 		public int FilteredWorkoutCount
 		{
 			get
 			{
-				if (null == WorkoutListFiltered)
+				if (null == WorkoutListFilteredSorted)
 				{
 					return 0;
 				}
-				return WorkoutListFiltered.Count();
+				return WorkoutListFilteredSorted.Count();
 			}
 			private set	{}
 		}
 
-		private List<IXertWorkout> _workoutListFiltered;
+		private List<IXertWorkout> _workoutListFilteredSorted;
 		
 		/// <summary>
 		/// This is a subset of _allWorkouts. It may be filtered and it may be sorted.
 		/// </summary>
-		public List<IXertWorkout> WorkoutListFiltered
+		public List<IXertWorkout> WorkoutListFilteredSorted
 		{
 			get
 			{
-				return _workoutListFiltered;
+				return _workoutListFilteredSorted;
 			}
 			private set
 			{
-				_workoutListFiltered = value;
-				OnPropertyChanged(nameof(WorkoutListFiltered));
+				_workoutListFilteredSorted = value;
+				OnPropertyChanged(nameof(WorkoutListFilteredSorted));
 				OnPropertyChanged(nameof(FilteredWorkoutCount));
 			}
 		}
@@ -244,10 +257,12 @@ namespace XertExplorer.ViewModels
 			AssendingSortDirection = true;
 			WorkoutsLoaded = false;
 			LoggedOff = true;
-			Filters = new List<string>(); 
+			FocusFilters = new List<string>();
+			DurationFilters = new List<string>();
 			LoadDemoWorkouts();
 
-			FilterCommand = new RelayCommand(ExecuteFilterMethod, CanexecuteFilterMethod);
+			FocusFilterCommand = new RelayCommand(ExecuteFocusFilterMethod, CanexecuteFilterMethod);
+			DurationFilterCommand = new RelayCommand(ExecuteDurationFilterMethod, CanexecuteFilterMethod);
 			SortCommand = new RelayCommand(ExecuteSortMethod, CanSortMethod);
 			LoginCommand = new AsyncRelayCommand(Login, (ex) => StatusMessage = ex.Message);
 			ToggleSortDirection = new RelayCommand(ToggleSortOrderDirection, CanToggleSortOrderDirection);
@@ -272,11 +287,10 @@ namespace XertExplorer.ViewModels
 
 		private void LoadDemoWorkouts()
 		{
-			_allWorkOuts = DeserializeWorkoutsFromFile("workouts.json");
-			WorkoutListFiltered = _allWorkOuts;
+			_allWorkouts = DeserializeWorkoutsFromFile("workouts.json");
 			WorkoutsLoaded = true;
-			ApplyFiltering();
-			StatusMessage = string.Format("{0} Demo Workouts Loaded", _allWorkOuts.Count());
+			ApplyAllFiltering();
+			StatusMessage = string.Format("{0} Demo Workouts Loaded", _allWorkouts.Count());
 		}
 
 		static List<IXertWorkout> DeserializeWorkoutsFromFile(string fileName)
@@ -296,11 +310,10 @@ namespace XertExplorer.ViewModels
 				return;
 			}
 			StatusMessage = "Loading Workouts...";
-			_allWorkOuts = await _client.GetUsersWorkouts();
-			WorkoutListFiltered = _allWorkOuts;
+			_allWorkouts = await _client.GetUsersWorkouts();
 			WorkoutsLoaded = true;
-			ApplyFiltering();
-			StatusMessage = string.Format("{0} Workouts Loaded", _allWorkOuts.Count());
+			ApplyAllFiltering();
+			StatusMessage = string.Format("{0} Workouts Loaded", _allWorkouts.Count());
 		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
@@ -308,7 +321,7 @@ namespace XertExplorer.ViewModels
 		{
 			if(propertyname == "SearchString")
 			{
-				ApplyFiltering();
+				ApplySorting( ApplySearchFilter(_focusFilteredWorkouts));
 			}
 			if (PropertyChanged != null)
 			{
@@ -323,112 +336,211 @@ namespace XertExplorer.ViewModels
 		/// <returns></returns>
 		private bool CanexecuteFilterMethod(object parameter)
 		{
-			if (null == _allWorkOuts )
+			if (null == _allWorkouts )
 			{
 				return false;
 			}
-			if(_allWorkOuts.Count == 0)
+			if(_allWorkouts.Count == 0)
 			{
 				return false;
 			}
 			return true;
 		}
-		
-		private void ApplyFiltering()
+
+		/// <summary>
+		/// Sets _focusFilteredWorkouts to a subset of _allWorkouts that is filtered by FocusFilters. If FocusFilters is empty,
+		/// _focusFilteredWorkouts  is set to _allWorkouts.
+		/// </summary>
+		/// <returns></returns>
+		private void ApplyFocusFilter()
 		{
-			if (Filters.Count > 0)
+			if (FocusFilters.Count > 0)
 			{
-				List<IXertWorkout> filteredItems = new List<IXertWorkout>();
-				foreach (string filer in Filters)
+				_focusFilteredWorkouts = new List<IXertWorkout>();
+				foreach (string filer in FocusFilters)
 				{
-					filteredItems = _allWorkOuts.Where(X => X.focus == filer).ToList();
+					_focusFilteredWorkouts.AddRange(_allWorkouts.Where(X => X.focus == filer));
 				}
-				if (!string.IsNullOrEmpty(SearchString))
-				{
-					filteredItems = filteredItems.Where(w => w.name.Contains(SearchString, StringComparison.OrdinalIgnoreCase) 
-						|| w.description.Contains(SearchString, StringComparison.OrdinalIgnoreCase)).ToList();
-				}
-				WorkoutListFiltered = filteredItems;
+				return;
+			}
+			_focusFilteredWorkouts =  _allWorkouts;
+		}
+
+		/// <summary>
+		/// returns a subset of the input list, that contains SearchString in the name or description. If SearchString is empty
+		/// returns inputWorkouts
+		/// </summary>
+		/// <param name="inputWorkouts"></param>
+		/// <returns></returns>
+		private List<IXertWorkout> ApplySearchFilter(List<IXertWorkout> inputWorkouts)
+		{
+			List<IXertWorkout> searchFiltered = new List<IXertWorkout>();
+			if (!string.IsNullOrEmpty(SearchString))
+			{
+				searchFiltered = inputWorkouts.Where(w => w.name.Contains(SearchString, StringComparison.OrdinalIgnoreCase)
+					|| w.description.Contains(SearchString, StringComparison.OrdinalIgnoreCase)).ToList();
+				return searchFiltered;
+			}
+			return inputWorkouts;
+		}
+
+		private TimeSpan StringToTimeSpan(string input)
+		{
+			TimeSpan intervalVal;
+			if (TimeSpan.TryParse(input, out intervalVal))
+			{
+				return intervalVal;
 			}
 			else
 			{
-				List<IXertWorkout> filteredItems = new List<IXertWorkout>();
-				if (!string.IsNullOrEmpty(SearchString))
-				{
-					filteredItems = _allWorkOuts.Where(w => w.name.Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-						|| w.description.Contains(SearchString, StringComparison.OrdinalIgnoreCase)).ToList();
-					WorkoutListFiltered = filteredItems;
-				}
-				else 
-				{
-					WorkoutListFiltered = _allWorkOuts;
-				}
+				MessageBox.Show(string.Format("Error converting {0} to TimeSpan", input));
+				return new TimeSpan(0,0,0);
 			}
-			ApplySorting();
 		}
 
-		private void ApplySorting()
+		private List<IXertWorkout> ApplyDurationFilter(List<IXertWorkout> inputWorkouts)
 		{
-			List<IXertWorkout> sortedWorkouts = WorkoutListFiltered;
+			List<IXertWorkout> durationFilteredWorkouts = new List<IXertWorkout>();
+			if (DurationFilters.Count > 0)
+			{
+				foreach (string durationFilter in DurationFilters)
+				{
+					if (durationFilter.Contains("Less than", StringComparison.OrdinalIgnoreCase))
+					{
+						string parsedDurationStr = Regex.Replace(durationFilter, "Less than", "", RegexOptions.IgnoreCase);
+						TimeSpan parsedDuration = StringToTimeSpan(parsedDurationStr);
+						durationFilteredWorkouts.AddRange(inputWorkouts.Where(X => TimeSpan.Parse(X.duration) < parsedDuration));
+					}
+					else if (durationFilter.Contains("More than", StringComparison.OrdinalIgnoreCase))
+					{
+						string parsedDurationStr = Regex.Replace(durationFilter, "More than", "", RegexOptions.IgnoreCase);
+						TimeSpan parsedDuration = StringToTimeSpan(parsedDurationStr);
+						durationFilteredWorkouts.AddRange(inputWorkouts.Where(X => TimeSpan.Parse(X.duration) > parsedDuration));
+					}
+					else
+					{
+						TimeSpan parsedTimeSpan = StringToTimeSpan(durationFilter);
+						TimeSpan low = parsedTimeSpan - (.5 * _minuteIncrementTimeSpan);
+						if(low <= _lowLimTimeSpan)
+						{
+							low = _lowLimTimeSpan;
+						}
+						TimeSpan high = parsedTimeSpan + (.5 * _minuteIncrementTimeSpan);
+						if (high >= _highLimTimeSpan)
+						{
+							high = _highLimTimeSpan;
+						}
+						durationFilteredWorkouts.AddRange(inputWorkouts.Where(X => TimeSpan.Parse(X.duration) >= low && TimeSpan.Parse(X.duration) <= high));
+					}
+				}
+				return durationFilteredWorkouts;
+			}
+			return inputWorkouts;
+		}
+
+		/// <summary>
+		/// Applies filtering to _allWorkouts and sorts the resulting list before setting it WorkoutListFilteredSorted
+		/// </summary>
+		private void ApplyAllFiltering()
+		{
+			ApplyFocusFilter();
+			List<IXertWorkout> focusAndSearchFilteredWOs = ApplySearchFilter(_focusFilteredWorkouts);
+			List<IXertWorkout> focusSearchDurFilteredWOs = ApplyDurationFilter(focusAndSearchFilteredWOs);
+			ApplySorting(focusSearchDurFilteredWOs);
+		}
+
+		/// <summary>
+		/// This sets WorkoutListFilteredSorted to be displayed on the UI after sorting per _currentSortingParam
+		/// </summary>
+		/// <param name="workoutLstFiltered"></param>
+		private void ApplySorting(List<IXertWorkout> workoutLstFiltered)
+		{
+			List<IXertWorkout> sortedWorkouts = new List<IXertWorkout>();
 			if (_currentSortingParam == SortingParam.Name)
 			{
 				if (AssendingSortDirection)
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderBy(o => o.name).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderBy(o => o.name).ToList();
 				}
 				else
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderByDescending(o => o.name).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderByDescending(o => o.name).ToList();
 				}
 			}
-			if (_currentSortingParam == SortingParam.Duration)
+			else if (_currentSortingParam == SortingParam.Duration)
 			{
 				if (AssendingSortDirection)
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderBy(o => o.duration).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderBy(o => o.duration).ToList();
 				}
 				else
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderByDescending(o => o.duration).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderByDescending(o => o.duration).ToList();
 				}
 			}
-			if (_currentSortingParam == SortingParam.XSS)
+			else if (_currentSortingParam == SortingParam.XSS)
 			{
 				if (AssendingSortDirection)
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderBy(o => o.xss).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderBy(o => o.xss).ToList();
 				}
 				else
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderByDescending(o => o.xss).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderByDescending(o => o.xss).ToList();
 				}
 			}
-			if (_currentSortingParam == SortingParam.Difficulty)
+			else if (_currentSortingParam == SortingParam.Difficulty)
 			{
 				if (AssendingSortDirection)
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderBy(o => o.difficulty).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderBy(o => o.difficulty).ToList();
 				}
 				else
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderByDescending(o => o.difficulty).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderByDescending(o => o.difficulty).ToList();
 				}
 			}
-			if (_currentSortingParam == SortingParam.AdvisorScore)
+			else if (_currentSortingParam == SortingParam.AdvisorScore)
 			{
 				if (AssendingSortDirection)
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderBy(o => o.advisorScore).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderBy(o => o.advisorScore).ToList();
 				}
 				else
 				{
-					sortedWorkouts = WorkoutListFiltered.OrderByDescending(o => o.advisorScore).ToList();
+					sortedWorkouts = workoutLstFiltered.OrderByDescending(o => o.advisorScore).ToList();
 				}
 			}
-			WorkoutListFiltered = sortedWorkouts;
+			else
+			{
+				sortedWorkouts = workoutLstFiltered;
+			}
+			WorkoutListFilteredSorted = sortedWorkouts;
 		}
 
-		private void ExecuteFilterMethod(object parameter)
+		private void ExecuteDurationFilterMethod(object parameter)
+		{
+			if (null != parameter)
+			{
+				var values = (object[])parameter;
+				string durationFilter = (string)values[0];
+				bool check = (bool)values[1];
+				if (check)
+				{
+					DurationFilters.Add(durationFilter);
+					OnPropertyChanged(nameof(DurationFilters));
+				}
+				else
+				{
+					DurationFilters.Remove(durationFilter);
+					OnPropertyChanged(nameof(DurationFilters));
+				}
+			}
+
+			ApplyAllFiltering();
+		}
+
+		private void ExecuteFocusFilterMethod(object parameter)
 		{
 			if(null != parameter)
 			{
@@ -437,17 +549,16 @@ namespace XertExplorer.ViewModels
 				bool check = (bool)values[1];
 				if (check)
 				{
-					Filters.Add(focusFilter);
-					OnPropertyChanged(nameof(Filters));
+					FocusFilters.Add(focusFilter);
+					OnPropertyChanged(nameof(FocusFilters));
 				}
 				else
 				{
-					Filters.Remove(focusFilter);
-					OnPropertyChanged(nameof(Filters));
+					FocusFilters.Remove(focusFilter);
+					OnPropertyChanged(nameof(FocusFilters));
 				}
 			}
-			
-			ApplyFiltering();
+			ApplyAllFiltering();
 		}
 
 		private void ExecuteSortMethod(object parameter)
@@ -477,16 +588,16 @@ namespace XertExplorer.ViewModels
 			{
 				_currentSortingParam = SortingParam.None;
 			}
-			ApplySorting();
+			ApplySorting(WorkoutListFilteredSorted);
 		}
 		
 		private bool CanToggleSortOrderDirection(object parameter)
 		{
-			if (null == WorkoutListFiltered)
+			if (null == WorkoutListFilteredSorted)
 			{
 				return false;
 			}
-			if (WorkoutListFiltered.Count == 0)
+			if (WorkoutListFilteredSorted.Count == 0)
 			{
 				return false;
 			}
@@ -500,11 +611,11 @@ namespace XertExplorer.ViewModels
 
 		private bool CanSortMethod(object parameter)
 		{
-			if (null == WorkoutListFiltered)
+			if (null == WorkoutListFilteredSorted)
 			{
 				return false;
 			}
-			if (WorkoutListFiltered.Count == 0)
+			if (WorkoutListFilteredSorted.Count == 0)
 			{
 				return false;
 			}
